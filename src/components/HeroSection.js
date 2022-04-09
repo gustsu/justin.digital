75b83e333fd61f41a598ad098b6533a94ba39808
Justin Tew <justintewtew@gmail.com>
2022-04-09 11:55:54 -0700

    progress on HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,24 @@
 import Container from 'react-bootstrap/Container';
+import linkedinIcon from '../icons/icon-linkedin.svg';
+import githubIcon from '../icons/icon-github.svg';
+import dribbbleIcon from '../icons/icon-dribbble.svg';
+import mailIcon from '../icons/icon-mail.svg';
 import './HeroSection.scss';
 
 export default function HeroSection() {
   return (
     <div className='HeroSection'>
         <Container>
-            <h1>Justin Tew</h1>
-            <h2>Frontend Developer</h2>
+            <div className="inner-content">
+              <h1>Justin Tew</h1>
+              <h2>Frontend Developer</h2>
+              <div className='icon-section'>
+                <img src={linkedinIcon} className="linkedin-logo" alt="LinkedIn Social Logo" />
+                <img src={githubIcon} className="linkedin-logo" alt="LinkedIn Social Logo" />
+                <img src={dribbbleIcon} className="linkedin-logo" alt="LinkedIn Social Logo" />
+                <img src={mailIcon} className="linkedin-logo" alt="LinkedIn Social Logo" />
+              </div>
+            </div>
         </Container>
     </div>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,8 +11,10 @@ export default function NavBar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#home">Contact</Nav.Link>
+                    <Nav.Link href="#link">Resume</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
